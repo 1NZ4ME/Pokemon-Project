86ac5fc6c3e0d0088abac2298c95f7be3ad9e3c3
1NZ4ME <yrijsenbrij@gmail.com>
2025-06-04 17:15:35 +0900

    check color

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -3756,7 +3756,7 @@ static void PrintRibbonCount(void)
 static void BufferStat(u8 *dst, u8 statIndex, u32 stat, u32 strId, u32 n)
 {
     static const u8 sTextNatureDown[] = _("{COLOR}{05}");
-    static const u8 sTextNatureUp[] = _("{COLOR}{02}");
+    static const u8 sTextNatureUp[] = _("{COLOR}{03}");
     static const u8 sTextNatureNeutral[] = _("{COLOR}{01}");
     u8 *txtPtr;
 
